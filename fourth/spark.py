3e8d857c10f3cd22f99b5b430539ccdaa5a0ec99
dvirbo <dvir.borochov@gmail.com>
2022-06-07 15:11:18 +0300

    finish all

--- fourth/spark.py
+++ fourth/spark.py
@@ -1,31 +1,20 @@
 from pyspark import SparkContext
 import findspark
 from pyspark.sql import SQLContext
-from pyspark.sql.functions import col, expr
+from pyspark.sql.functions import col
 
 findspark.init()
 sc = SparkContext("local[*]", "Simple App")
 sqlContext = SQLContext(sc)
 my_year = 2022
 
+# start q1:
 df = sqlContext.read.json("books.json", multiLine=True)
-ndf = df.filter(col("author").startswith("F")).select("title", "author", "year").show()  # q1
-'''
-rdd = ndf.rdd
-sub = rdd.reduce(lambda row: my_year - row.year).collect()
-finish = sqlContext.createDataFrame(sub).show()
-'''
+ndf = df.filter(col("author").startswith("F")).select("title", "author", "year").withColumn("year", my_year - col(
+    "year")).show()
+# end q1
 
 
-
-''' # q2
-df.filter(col("language").startswith("English")).select("pages", "author") 
-rdd = df.rdd
-calc_amount = rdd.map(lambda row: (row.author, row.pages)) \
-    .reduceByKey(lambda a, b: a + b) \
-    .collect()
-
-for author, sum_amount in calc_amount:
-    print('%s : %s' % (author, sum_amount))
-
-'''
+# start q2
+df.filter(col("language").startswith("English")).select("pages", "author").groupBy("author").avg("pages").show()
+# end q2
